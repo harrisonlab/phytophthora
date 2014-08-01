f088c0b67c5f94ed4603f1bcfb0952efd8697d16
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-08-01 11:38:36 +0100

    run_blast_pipe.sh is a wrapper to submit jobs to pathogen/blast_pipe.sh. Performing blast homology searching of a query file of genes against each other to identify homologous groups and to also blast these genes against assembled genomes.

---
+++ homology_searching/run_blast_pipe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+QUERY=analysis/blast_homology/PHI_36_accessions
+
+PROGRAM=/home/armita/git_repos/emr_repos/tools/pathogen/blast/blast_pipe.sh 
+
+qsub $PROGRAM $QUERY assembly/velvet/P.cactorum/404/404_assembly.41/sorted_contigs.fa
+
+qsub $PROGRAM $QUERY assembly/velvet/P.cactorum/414/414_assembly.41/sorted_contigs.fa 
+
+qsub $PROGRAM $QUERY assembly/velvet/P.ideai/371/371_assembly.41/sorted_contigs.fa 
+
+qsub $PROGRAM $QUERY assembly/genbank/P.fragariae/JHVZ02/assembly_version2/JHVZ02.1.fsa_nt
+
+qsub $PROGRAM $QUERY assembly/kamoun_lab/P.cactorum/10300/version1/Pcactorum_contigs_singletons_14436.fan.txt
+
