6d182f20dfe13fb5430f78ef04385edae22a4d9b
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-08-08 15:08:21 +0100

    As run on 06.08.14

--- homology_searching/run_blast_pipe.sh
+++ homology_searching/run_blast_pipe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-QUERY=analysis/blast_homology/PHI_36_accessions
+QUERY=analysis/blast_homology/rxlr_sp/appended_rxlr_sp.fa 
 
 PROGRAM=/home/armita/git_repos/emr_repos/tools/pathogen/blast/blast_pipe.sh 
 
@@ -8,9 +8,10 @@ qsub $PROGRAM $QUERY assembly/velvet/P.cactorum/404/404_assembly.41/sorted_conti
 
 qsub $PROGRAM $QUERY assembly/velvet/P.cactorum/414/414_assembly.41/sorted_contigs.fa 
 
+qsub $PROGRAM $QUERY assembly/kamoun_lab/P.cactorum/10300/version1/Pcactorum_contigs_singletons_14436.fa
+
 qsub $PROGRAM $QUERY assembly/velvet/P.ideai/371/371_assembly.41/sorted_contigs.fa 
 
 qsub $PROGRAM $QUERY assembly/genbank/P.fragariae/JHVZ02/assembly_version2/JHVZ02.1.fsa_nt
 
-qsub $PROGRAM $QUERY assembly/kamoun_lab/P.cactorum/10300/version1/Pcactorum_contigs_singletons_14436.fan.txt
 
