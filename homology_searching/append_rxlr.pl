044430e054ee4797bc865ada91fdf12900034b21
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-08-08 15:06:40 +0100

    Modified to also parse P.fragaria JHZ02 sigP rxlr file into readable fasta format

--- homology_searching/append_rxlr.pl
+++ homology_searching/append_rxlr.pl
@@ -8,12 +8,18 @@ my $line;
 for (@ARGV) {
 	my @nameparts = split ('/', $_);
 	my $genome = $nameparts[-3];
+	$genome =~ s/\./_/g; 
 	my $isolate = $nameparts[-2];
 	my $file = $_;
 	open (INFILE, "$file") or die $usage;
 	while ($line = <INFILE>) {
-		$line =~ s/>/>$genome\|$isolate\|/g;
+#		$line =~ s/>/>$genome\|$isolate\|/g;
+#		$line =~ s/>/>lcl\|$genome--$isolate--/g;
+#		$line =~ s/\./_/g;
+		$genome =~ s/\|/_/g;
+		$line =~ s/>/>$genome--$isolate--/g;
 		$line =~ s/\s//g;
+		$line =~ s/gi\|(.*)\|gb\|(.*)\|Phytophthorafragariae/$1--$2--/g; 
 		print "$line\n";
 	}
 	close (INFILE);
