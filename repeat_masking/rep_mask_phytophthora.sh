72f9a607488c15c350ffef9f9a1fd9528d89957c
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-09-26 18:53:47 +0100

    added commands for transposon_psi.pl

--- repeat_masking/rep_mask_phytophthora.sh
+++ repeat_masking/rep_mask_phytophthora.sh
@@ -5,3 +5,6 @@ qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modelin
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/P.ideai/371/371_assembly.41/sorted_contigs.fa
 
 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/P.cactorum/404/404_assembly.51/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/P.cactorum/414/414_assembly.61/sorted_contigs.fa 
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/P.ideai/371/371_assembly.41/sorted_contigs.fa
