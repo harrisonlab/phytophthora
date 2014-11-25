6e505fa24d9ef75b868167a7f29d1187ae3afabc
adarmitage <Andrew.Armitage@EMR.ac.uk>
2014-11-25 14:07:39 +0000

    now includes repeat masking for P. frag genome with renamed contigs

--- repeat_masking/rep_mask_phytophthora.sh
+++ repeat_masking/rep_mask_phytophthora.sh
@@ -4,10 +4,13 @@ qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modelin
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/P.cactorum/414/414_assembly.61/sorted_contigs.fa 
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/P.ideai/371/371_assembly.41/sorted_contigs.fa
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/kamoun_lab/P.cactorum/10300/version1/Pcactorum_contigs_singletons_14436.fa 
-qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/genbank/P.fragariae/JHVZ02/assembly_version2/JHVZ02.1.fsa_nt
+#qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/genbank/P.fragariae/JHVZ02/assembly_version2/JHVZ02.1.fsa_nt
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/genbank/P.fragariae/JHVZ02/assembly_version2_ed/JHVZ02_renamed.fa
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/P.fragariae/SCRP245/SCRP245_assembly.61/sorted_contigs.fa
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/rep_modeling.sh assembly/velvet/P.cactorum/411/411_assembly.41/sorted_contigs.fa
 
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/P.cactorum/404/404_assembly.51/sorted_contigs.fa 
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/P.cactorum/414/414_assembly.61/sorted_contigs.fa 
 qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/velvet/P.ideai/371/371_assembly.41/sorted_contigs.fa
+
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/repeat_masking/transposonPSI.sh assembly/genbank/P.fragariae/JHVZ02/assembly_version2_ed/JHVZ02_renamed.fa
