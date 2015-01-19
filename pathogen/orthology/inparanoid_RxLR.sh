48bdbd8d912589fbdec11d7b61ae1377f835a295
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-01-19 17:54:17 +0000

    Added explanation of results from analysis. Results indicate that the INPARANOID scripts are doing what we think they are

--- pathogen/orthology/inparanoid_RxLR.sh
+++ pathogen/orthology/inparanoid_RxLR.sh
@@ -25,3 +25,29 @@ cat analysis/inparanoid/*/*_seqs.txt | cut -f1 | sort | uniq > analysis/inparano
 
 cat analysis/inparanoid/10300-404/*_seqs.txt analysis/inparanoid/10300-414/*_seqs.txt analysis/inparanoid/404-414/*_seqs.txt | cut -f1 | sort | uniq > analysis/inparanoid/summary_tables/all_P.cac_RxLR.txt
 /home/armita/git_repos/emr_repos/tools/pathogen/orthology/inparanoid/build_orthology_tab.pl analysis/inparanoid/summary_tables/all_P.cac_RxLR.txt analysis/inparanoid/10300-404/sqltable.10300-404 analysis/inparanoid/10300-414/sqltable.10300-414 analysis/inparanoid/404-414/sqltable.404-414 > analysis/inparanoid/summary_tables/rxlr_orthology_P.cact.csv
+
+# 404 = 1024 genes, 1016 ortholog groups - 8 genes different
+# 414 = 1048 genes, 1036 ortholog groups - 12 genes different
+# 10300 = 1144 genes, 1054 ortholog groups - 90 genes different
+
+# Identify which genes are present as inparalogs.
+# 404 = 1x3copy 2x2copy
+# 414 = 1x3copy, 4x2copy
+# 10300 = 45x2 copy
+for FILE in $(ls analysis/inparanoid/summary_tables/orthogroups4/*.txt); do 
+	cat $FILE | cut -f1 -d '|' | uniq -dc; printf "$FILE\n"
+done | grep -B1 '414' | less
+
+# identify the number of genes that are represented by orthogroups from 10300
+cat analysis/inparanoid/summary_tables/new_final_tab.csv analysis/rxlr_unmasked/P.cactorum/10300/10300_sp_rxlr.fa | grep -o '10300.*' | cut -f1 | sed s'/\.txt//' | sed s'/_singleton_insl.*bp//' | sed 's/_Pcac10300//' | sed 's/_contig//' | sed 's/ //' | tail -n+2 | sort | uniq -d | wc -l
+# 1054 = this number will not take account of genes that are duplicated within an orthogroup.
+# identify the number of genes that are not present in the orthogroups in the outfile.
+cat analysis/inparanoid/summary_tables/new_final_tab.csv analysis/rxlr_unmasked/P.cactorum/10300/10300_sp_rxlr.fa | grep -o '10300.*' | cut -f1 | sed s'/\.txt//' | sed s'/_singleton_insl.*bp//' | sed 's/_Pcac10300//' | sed 's/_contig//' | sed 's/ //' | tail -n+2 | sort | uniq -u | wc -l
+# 90 genes different
+
+# Understanding the difference:
+# 404 = 1x3copy, 2x2copy ~ 4 genes missing from analysis & 4 genes overpredicted 
+# 414 = 1x3copy, 4x2copy ~ 6 genes missing from analysis and 6 genes overpredicted
+# 10300 = 45x2 copy ~ 45 genes missing from analysis and 45 genes overpredicted
+
+
