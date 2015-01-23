54b756812fd15883fd90e5e776cf74813bb52f66
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-01-23 17:54:53 +0000

    Training gene models for phytophthora using cactorum RNAseq data

---
+++ gene_prediction/training_models_P.cact.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Training augustus for P. catcorum, P. idaei and P. fragariae
+
+#qsub /home/armita/git_repos/emr_repos/scripts/alternaria/gene_pred/Gene_pred_pipe.sh raw_rna/genbank/mixed_species/mixed_strains/phytophthora_ESTs.fasta repeat_masked/P.cactorum/404/404_assembly.51_repmask/404_contigs_hardmasked.fa
+
+
+fastq-dump raw_rna/genbank/P.cactorum/10300/SRR1206032.sra
+mv SRR1206032.fastq raw_rna/genbank/P.cactorum/10300/.
+fastq-dump raw_rna/genbank/P.cactorum/10300/SRR1206033.sra 
+mv SRR1206033.fastq raw_rna/genbank/P.cactorum/10300/.
+fastq-dump --split-3 raw_rna/genbank/P.cactorum/10300/SRR1206032.sra -O raw_rna/genbank/P.cactorum/10300/
+fastq-dump --split-3 raw_rna/genbank/P.cactorum/10300/SRR1206033.sra -O raw_rna/genbank/P.cactorum/10300/
+qsub /home/armita/git_repos/emr_repos/tools/seq_tools/rna_qc/rna_qc_fastq-mcf.sh raw_rna/genbank/P.cactorum/10300/SRR1206032.fastq raw_rna/genbank/P.cactorum/10300/SRR1206033.fastq /home/armita/git_repos/emr_repos/tools/seq_tools/illumina_full_adapters.fa 
