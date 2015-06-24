046b98a793de86d9cb54e3ea9a554629d41aec76
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-06-24 19:14:38 +0100

    Added transformation to give features an ID attribute if missing

--- pathogen/merge_gff/make_gff_database.py
+++ pathogen/merge_gff/make_gff_database.py
@@ -6,6 +6,7 @@ make a gffutils db from a gff file
 
 import sys,argparse
 import gffutils
+i = 0
 
 ap = argparse.ArgumentParser(description=__doc__,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 ap.add_argument('--inp',required=True,type=str,help='input gff file')
@@ -14,11 +15,26 @@ conf = ap.parse_args() #sys.argv
 
 #db = gffutils.create_db(conf.inp, force=True, dbfn=conf.db)
 
+
+def transform_func(x):
+    # adds some text to the end of transcript IDs
+	if not 'ID' in x.attributes:
+# 		i += 1
+ 		new_id = "".join(x.attributes['Parent']) + "." + str(x.featuretype)
+# 		print(new_id)
+		x.attributes['ID'] = new_id
+#	x.attributes['gene_name'] = 'spoons'
+	return x
+
+
 db1 = gffutils.create_db(
-	conf.inp, 
+	conf.inp,
 	dbfn=conf.db,
-	force=True, 
+	force=True,
 	keep_order=True, 
-	merge_strategy='merge', 
+	merge_strategy='merge',
+	transform=transform_func, 
 	sort_attribute_values=True
 	)
+
+
