a5e1ca91a2cd6c0fdaf56023434e95e06fc49a5d
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-06-25 10:29:39 +0100

    Now performs transformation function when creating the databaseto rename all sources as a given string. This aids mergeing from files with different listed sources.

--- pathogen/merge_gff/merge_db.py
+++ pathogen/merge_gff/merge_db.py
@@ -19,6 +19,7 @@ from itertools import chain
 ap = argparse.ArgumentParser(description=__doc__,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 ap.add_argument('--inp',required=True,type=str,nargs='+',help='list of databases to input')
 ap.add_argument('--db',required=True,type=str,help='output db file')
+ap.add_argument('--source',required=False,type=str,help='(Optional) String to replace the source column - aids merging.')
 conf = ap.parse_args() #sys.argv
 
 
@@ -44,10 +45,34 @@ gff_str = "\n".join(all_gff)
 #                                     #
 #######################################
 
+new_source = conf.source
+def transform_func(x):
+   # Replace the source column with text from stdin
+	if new_source:
+		x.source = new_source
+	return x
+# 	if not 'ID' in x.attributes:
+#  		new_id = "".join(x.attributes['Parent']) + "." + str(x.featuretype)
+# 		if id_dict.has_key(new_id):
+# 			id_dict[new_id] += 1
+# 		else:
+# 			id_dict[new_id] = 1
+# 		x.attributes['ID'] = new_id + str(id_dict[new_id])
+# 	return x
+
 out_db = conf.db
 print("Creting the merged database:\n" + out_db)
-db3 = gffutils.create_db(gff_str, from_string=True, dbfn=out_db, force=True, keep_order=False, 
-	sort_attribute_values='merge', merge_strategy='merge', id_spec=['ID'])
+db3 = gffutils.create_db(
+	gff_str,
+	from_string=True,
+	dbfn=out_db,
+	force=True,
+	keep_order=False, 
+	sort_attribute_values='merge',
+	transform=transform_func, 
+	merge_strategy='merge',
+	id_spec=['ID']
+	)
 
 quit()
 
