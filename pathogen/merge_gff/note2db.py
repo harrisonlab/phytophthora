f22f4534dafc91b1f875c834a047e40bde7076a2
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-06-25 15:23:17 +0100

    Correctly uses input id names for features

--- pathogen/merge_gff/note2db.py
+++ pathogen/merge_gff/note2db.py
@@ -24,6 +24,11 @@ ap.add_argument('--id_file',required=True,type=str,help='A text file containing 
 ap.add_argument('--str',required=True,type=str,help='Text to add to the notes section for each matched feature.')
 conf = ap.parse_args() #sys.argv
 
+#######################################
+#      Load the ID file into          #
+#             a dict.                 #
+#                                     #
+#######################################
 
 dict = {}
 with open(conf.id_file) as f:
@@ -38,52 +43,37 @@ with open(conf.id_file) as f:
 #######################################
 
 new_note = conf.str
+
+# def transform_func(x):
+# 	if 'ID' in x.attributes:
+# 		this_id = "".join(x.attributes['ID'])
+# 		if this_id in dict.keys():
+# #			print('badgers')
+# 			if not 'Note' in x.attributes:
+# 				x.attributes['Note'] = new_note
+# 			else:
+# 				x.attributes['Note'].append(new_note)
+# 			print(str(x))
+# 	return x
+	
 def transform_func(x):
-   # Replace the source column with text from stdin
-	if not 'Note' in in_db[id].attributes:
-#		in_db[id].attributes['Note'] += "," + new_note
-		x.attributes['Note'] = new_note
-	else:
-#		in_db[id].attributes['Note'] = "monkeys"
-		x.attributes['Note'].append(new_note)
-	print(str(x))
+	this_id = "".join(x.attributes['ID'])
+	if this_id in dict.keys():
+		if not 'Note' in x.attributes:
+			x.attributes['Note'] = new_note
+		else:
+			x.attributes['Note'].append(new_note)
+		print(str(x))
 	return x
 
 #######################################
-#        Search for the provided      #
-#               strings &             #
-#         output to a gff file        #
+#        Create output database       #
+#                                     #
+#                                     #
 #######################################
 
 f = conf.in_db
 in_db = gffutils.FeatureDB(f)
-
-
-# d = {}
-# new_note = conf.str
-# print("note to add:\t" + new_note)
-# with open(conf.id_file) as f:
-#     for line in f:
-#     	id = line.strip()
-#     	print(id)
-#     	print(in_db[id].attributes['ID'])
-#     	print(in_db[id].attributes.items())
-#     	in_db[id].start = 23
-#     	print(in_db[id].start)
-#     	print(in_db[id].attributes.items())    	
-# #    	in_db[id].attributes['Parent'].append('monkeys')
-# #    	print(in_db[id].attributes['Note'])
-# #    	print(in_db[id].attributes['Parent'])
-# #    	in_db[id].attributes['ID'].append('monkeys')
-# #    	print(str(in_db[id].attributes['ID'].append('monkeys')))
-# # #   	print(in_db[id])
-# #     	if in_db[id].attributes['ID']:
-# #     		in_db[id].attributes['ID'] += "," + new_note
-# #     	else:
-# #     		in_db[id].attributes['ID'] = "monkeys"
-# #     		#new_note
-# #    	print(in_db[id])
-
 
 gffutils.create_db(
 	in_db,
