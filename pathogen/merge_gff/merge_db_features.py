4bf9f465a0074067062e2169680536de93e42dc4
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-06-29 11:36:17 +0100

    Creates a gff database as output with additional option to print sequences to stdout. Command line options to name ID and source fields in gff.

--- pathogen/merge_gff/merge_db_features.py
+++ pathogen/merge_gff/merge_db_features.py
@@ -18,13 +18,16 @@ from itertools import chain
 ap = argparse.ArgumentParser(description=__doc__,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 ap.add_argument('--inp',required=True,type=str,help='databases to input')
-#ap.add_argument('--out',required=True,type=str,help='Name of output text document of IDs')
+ap.add_argument('--id',required=True,type=str,help='The ID for newly merged features will start with this string')
+ap.add_argument('--source',required=False,type=str,default='merge_db_features.py',help='A string describing the source of newly created features')
+ap.add_argument('--gff',required=False,action='store_true',help='Will print the output in .gff3 format to stdout if set')
+ap.add_argument('--out',required=True,type=str,help='The name of the output database.')
+
 conf = ap.parse_args() #sys.argv
 
 f = conf.inp
-#o = open(conf.out, 'w')
-
-
+o = open(conf.out, 'w')
+s = conf.gff
 
 #######################################
 #          Open the gffutils          #
@@ -40,9 +43,11 @@ db = gffutils.FeatureDB(f)
 #                                     #
 #######################################
 d = {}
+i = 0
+out_lines = []
 
 genes = db.features_of_type('gene')
-print(db.count_features_of_type('gene'))
+#print(db.count_features_of_type('gene'))
 # merged = db.merge(genes, ignore_strand=False)
 # for feature in merged:
 #       print(feature)
@@ -53,18 +58,41 @@ for gene in genes:
     if not ID in d.keys():
 #        print("bagers")
         strand = gene.strand
-        list = db.region(region = gene, strand=strand, featuretype = 'gene')
-        for feature in list:
+        overlaps = db.region(region = gene, strand=strand, featuretype = 'gene')
+        list = []
+        for feature in overlaps:
             x = "".join(feature.attributes['ID'])
             d[x] = ""
+            list.append(x)
             # print("badger")
             # print(feature)
-        list = db.region(region = gene, strand=strand, featuretype = 'gene')
-        merged = db.merge(list, ignore_strand=False)
+        overlaps = db.region(region = gene, strand=strand, featuretype = 'gene')
+        merged = db.merge(overlaps, ignore_strand=False)
         for new in merged:
-            print(new)
+            i += 1
+            new.source = str(conf.source)
+            new.attributes['ID'] = [str(conf.id) + "_" + str(i)]
+            new.attributes['Note'] = ['merged(' + " ".join(list) + ')']
+            out_lines.append(new)
+            if s == True:
+                print(new)
 
-#        print(merged)
+
+db3 = gffutils.create_db(
+	out_lines,
+	from_string=True,
+	dbfn=conf.out,
+	force=True,
+	keep_order=False,
+	sort_attribute_values='merge',
+#	transform=transform_func,
+	merge_strategy='merge',
+	id_spec=['ID']
+	)
+#write(out_lines, "\n")
+
+print(db3.count_features_of_type('gene'))
+
 
 
 
