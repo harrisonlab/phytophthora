77a194f392bbb5f89ce3f34646fce52feff88123
adarmitage <Andrew.Armitage@EMR.ac.uk>
2015-06-30 15:49:03 +0100

    Now outputs both a text file containing IDs of the overlapped genes and a database file of the merged features

--- pathogen/merge_gff/contained_features.py
+++ pathogen/merge_gff/contained_features.py
@@ -20,7 +20,9 @@ from itertools import chain
 ap = argparse.ArgumentParser(description=__doc__,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 ap.add_argument('--inp',required=True,type=str,help='databases to input')
-ap.add_argument('--out',required=True,type=str,help='Name of output text document of IDs')
+ap.add_argument('--out_txt',required=True,type=str,help='Name of output text document of IDs')
+ap.add_argument('--out_db',required=True,type=str,help='Name of output database containing the merged unique features')
+ap.add_argument('--gff',required=False,action='store_true',help='Will print the output in .gff3 format to stdout if set')
 ap.add_argument('--A',required=True,type=str,help='Genes that may encapsulate other genes are from source A')
 ap.add_argument('--B',required=True,type=str,help='Source of genes that may be encapsulated by genes from A')
 #ap.add_argument('--db',required=True,type=str,help='output db file')
@@ -28,13 +30,16 @@ ap.add_argument('--B',required=True,type=str,help='Source of genes that may be e
 conf = ap.parse_args() #sys.argv
 
 f = conf.inp
-o = open(conf.out, 'w')
+o_txt = open(conf.out_txt, 'w')
 a = conf.A
 b = conf.B
-out_lines = []
+# out_lines = []
+out_lines1 = []
 out_lines2 = []
 used_ids = []
 d = defaultdict(list)
+s = conf.gff
+merge_count = 0
 #regex = conf.str
 #out_f = open(conf.out, 'w')
 
@@ -65,14 +70,14 @@ for gene in genes:
         c = 0
         # start = []
         # end = []
-        start = ""
-        end = ""
+        # start = ""
+        # end = ""
         # B_ID = ""
         B_ID = []
         B_note = []
         B_name = []
         for contained_feature in list:
-            out_lines.append(str(contained_feature))
+            # out_lines.append(str(contained_feature))
             # print(contained_feature)
             if not a in contained_feature.source:
 #                print(contained_feature)
@@ -87,20 +92,24 @@ for gene in genes:
                 # start.append(contained_feature.start)
                 # end.append(contained_feature.end)
             else:
-                out_lines.append(str(contained_feature))
+                # out_lines.append(str(contained_feature))
 #                print(contained_feature)
                 A_ID = "".join(contained_feature.attributes['ID'])
-        out_lines.append("number of ORF_fragments in this gene:\t" + str(c))
+                # print("badgers\t" + A_ID)
+        # out_lines.append("number of ORF_fragments in this gene:\t" + str(c))
         # print("number of ORF_fragments in this gene:\t" + str(c))
-        if c > 1:
-            o.write(A_ID + "\n")
+        # if c > 1:
+        #     o_txt.write(A_ID + "\n")
+
         if c >= 1:
             # out_lines2.append("A consensus of these gene features may look like:")
             # print(str(B_ID))
             # gene.attributes['ID']
+            merge_count += 1
             gene['Note'] = B_note
             # B_name.append(B_ID)
             gene['Name'] = B_ID
+            out_lines1.append(str(A_ID))
             out_lines2.append(str(gene))
             used_ids.extend(gene['ID'])
             used_ids.extend(B_ID)
@@ -145,29 +154,28 @@ unmatched = 0
 for gene in genes:
     this_id = "".join(gene.attributes['ID'])
     if this_id in d:
+        # print(gene)
         # print("found gene:\t" + this_id)
         found += 1
     else:
         # print("unmatched gene\t\t" + this_id)
-        # print(gene)
+        #print(gene)
         unmatched += 1
         out_lines2.append(str(gene))
         for child in db.children(gene):
             out_lines2.append(str(child))
 
+for line in out_lines1:
+    o_txt.write(line + "\n")
+
 db_lines = "\n".join(out_lines2)
-print(db_lines)
+# print(db_lines)
 
-print("The total number of Augustus genes are:\t" + str(aug))
-print("The total number of atg genes are:\t" + str(atg))
-
-print("Of these, this many were merged:\t" + str(found))
-print("And this many remain unmerged:\t" + str(unmatched))
 
 db3 = gffutils.create_db(
 	db_lines,
 	from_string=True,
-	dbfn=conf.out,
+	dbfn=conf.out_db,
 	force=True,
 	keep_order=False,
 	sort_attribute_values='merge',
@@ -175,6 +183,21 @@ db3 = gffutils.create_db(
 	id_spec=['ID']
 	)
 
-
 merge_num = db3.count_features_of_type(featuretype='gene')
-print("The final dataset contains the following number of features:\t" + str(merge_num))
+
+if not s == True:
+    print("The total number of Augustus genes are:\t" + str(aug))
+    print("The total number of atg genes are:\t" + str(atg))
+    print("Of these, this many were merged:\t" + str(found))
+    print("Into this many features:\t" + str(merge_count))
+    print("And this many remain unmerged:\t" + str(unmatched))
+    print("The final dataset contains the following number of features:\t" + str(merge_num))
+
+if s == True:
+    print("##gff-version 3")
+    for feature in db3.all_features():
+        print(feature)
+# print("\n".join(out_lines1))
+# print(len(out_lines1))
+
+quit()
