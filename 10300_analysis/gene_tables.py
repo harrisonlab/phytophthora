f2d3d24484d85d9f52abf733e409c5530bee3695
adarmitage <Andrew.Armitage@EMR.ac.uk>
2017-01-13 11:00:44 +0000

    Script mow handles all input data files and works when gff3 or gtf format files are used

--- 10300_analysis/gene_tables.py
+++ 10300_analysis/gene_tables.py
@@ -17,6 +17,7 @@ from sets import Set
 from collections import defaultdict
 
 ap = argparse.ArgumentParser()
+ap.add_argument('--gff_format',required=True,type=str,choices=['gff3', 'gtf'],help='Gff file format')
 ap.add_argument('--gene_gff',required=True,type=str,help='Gff file of predicyted gene models')
 ap.add_argument('--gene_fasta',required=True,type=str,help='amino acid sequence of predicted proteins')
 ap.add_argument('--SigP2',required=True,type=str,help='fasta file of genes testing positive for signal peptide using SigP2.0')
@@ -27,6 +28,8 @@ ap.add_argument('--RxLR_Hmm',required=True,type=str,help='fasta file of genes te
 ap.add_argument('--RxLR_WY',required=True,type=str,help='fasta file of genes testing positive for WY domains using an hmm model')
 ap.add_argument('--CRN_LFLAK',required=True,type=str,help='fasta file of genes testing positive for LFLAK domains using an hmm model')
 ap.add_argument('--CRN_DWL',required=True,type=str,help='fasta file of genes testing positive for DWL domains using an hmm model')
+ap.add_argument('--ortho_name',required=True,type=str,help='the name used for the organism during orthology analysis')
+ap.add_argument('--ortho_file',required=True,type=str,help='txt file of ortholog groups')
 
 conf = ap.parse_args()
 
@@ -61,6 +64,9 @@ with open(conf.CRN_LFLAK) as f:
 
 with open(conf.CRN_DWL) as f:
     CRN_DWL_lines = f.readlines()
+
+with open(conf.ortho_file) as f:
+    ortho_lines = f.readlines()
 
 #-----------------------------------------------------
 # Load protein sequence data into a dictionary
@@ -167,49 +173,119 @@ for line in CRN_DWL_lines:
         CRN_DWL_set.add(header)
 
 #-----------------------------------------------------
+# Store genes and their ortholog groups in a dictionary
+#-----------------------------------------------------
+
+organism_name = conf.ortho_name
+ortho_dict = defaultdict(list)
+for line in ortho_lines:
+    line = line.rstrip()
+    split_line = line.split()
+    orthogroup = split_line[0]
+    orthogroup = orthogroup.replace(":", "")
+    genes_in_group = [ x for x in split_line if organism_name in x ]
+    for gene in genes_in_group:
+        gene = gene.replace(organism_name, '').replace('|', '')
+        # print gene
+        ortho_dict[gene] = orthogroup
+
+
+#-----------------------------------------------------
 # Step 3
 # Itterate through genes in file, identifying if
 # they ahve associated information
 #-----------------------------------------------------
+transcript_lines = []
 
-for line in gene_lines:
-    line = line.rstrip()
-    split_line = line.split()
-    if 'transcript' in split_line[2]:
-        # Set defaults
-        sigP2 = ''
-        sigP4 = ''
-        phobius = ''
-        RxLR_motif = ''
-        RxLR_hmm = ''
-        WY_hmm = ''
-        CRN_LFLAK = ''
-        CRN_DWL = ''
-        orthogroup = ''
-        prot_seq = ''
-        # Identify gene id
+if conf.gff_format == 'gff3':
+    for line in gene_lines:
+        line = line.rstrip()
+        if line.startswith('#'):
+            continue
+        split_line = line.split()
+        if 'transcript' in split_line[2] or 'mRNA' in split_line[2]:
+            transcript_lines.append("\t".join(split_line))
+
+if conf.gff_format == 'gtf':
+    prev_id = 'first'
+    transcript_line = ''
+
+    for line in gene_lines:
+        line = line.rstrip()
+        if line.startswith('#'):
+            continue
+        split_line = line.split("\t")
+        if 'CDS' in split_line[2]:
+            transcript_id = split_line[8]
+            split_col9 = split_line[8].split(';')
+            # print split_col9
+            transcript_id = "".join([ x for x in split_col9 if 'transcript_id' in x ])
+            # print transcript_id
+            transcript_id = transcript_id.replace(' ','').replace('transcript_id', '').replace('"', '')
+            # print transcript_id
+            if transcript_id != prev_id:
+                # if prev_id == 'first':
+                #     continue
+                transcript_lines.append("\t".join(transcript_line))
+                transcript_line = split_line
+                transcript_line[2] = "mRNA"
+                transcript_line[8] = transcript_id
+                # print split_line
+                # print transcript_line
+            elif split_line[6] == '+':
+                transcript_line[4] = split_line[4]
+            elif split_line[6] == '-':
+                transcript_line[3] = split_line[3]
+            # print "\t".join([prev_id, transcript_id])
+            prev_id = transcript_id
+            # print transcript_id
+    del transcript_lines[0]
+
+# print "\n".join(transcript_lines)
+
+for line in transcript_lines:
+    split_line = line.split("\t")
+    # Set defaults
+    sigP2 = ''
+    # sigP4 = ''
+    phobius = ''
+    RxLR_motif = ''
+    RxLR_hmm = ''
+    WY_hmm = ''
+    CRN_LFLAK = ''
+    CRN_DWL = ''
+    orthogroup = ''
+    prot_seq = ''
+    # Identify gene id
+    if 'ID' in split_line[8]:
+        split_col9 = split_line[8].split(';')
+        transcript_id = "".join([ x for x in split_col9 if 'ID' in x ])
+        transcript_id = transcript_id.replace('ID=', '')
+    else:
         transcript_id = split_line[8]
 
-        if transcript_id in SigP2_set:
-            sigP2 = 'Yes'
-        # if transcript_id in SigP4_set:
-        #     sigP4 = 'Yes'
-        if transcript_id in phobius_set:
-            phobius = 'Yes'
-        if transcript_id in RxLR_motif_set:
-            RxLR_motif = 'Yes'
-        if transcript_id in RxLR_hmm_set:
-            RxLR_hmm = 'Yes'
-        if transcript_id in RxLR_WY_set:
-            WY_hmm = 'Yes'
-        if transcript_id in CRN_LFLAK_set:
-            CRN_LFLAK = 'Yes'
-        if transcript_id in CRN_DWL_set:
-            CRN_DWL = 'Yes'
+    if transcript_id in SigP2_set:
+        sigP2 = 'Yes'
+    # if transcript_id in SigP4_set:
+    #     sigP4 = 'Yes'
+    if transcript_id in phobius_set:
+        phobius = 'Yes'
+    if transcript_id in RxLR_motif_set:
+        RxLR_motif = 'Yes'
+    if transcript_id in RxLR_hmm_set:
+        RxLR_hmm = 'Yes'
+    if transcript_id in RxLR_WY_set:
+        WY_hmm = 'Yes'
+    if transcript_id in CRN_LFLAK_set:
+        CRN_LFLAK = 'Yes'
+    if transcript_id in CRN_DWL_set:
+        CRN_DWL = 'Yes'
+    if ortho_dict[transcript_id]:
+        orthogroup = ortho_dict[transcript_id]
 
-        prot_seq = "".join(prot_dict[transcript_id])
-
-        outline = [transcript_id, sigP2, sigP4, phobius ,RxLR_motif, RxLR_hmm, WY_hmm, CRN_LFLAK, CRN_DWL, orthogroup]
-        outline.extend(split_line)
-        outline.append(prot_seq)
-        print "\t".join(outline)
+    prot_seq = "".join(prot_dict[transcript_id])
+    outline = [transcript_id, sigP2, phobius ,RxLR_motif, RxLR_hmm, WY_hmm, CRN_LFLAK, CRN_DWL, orthogroup]
+    # outline = [transcript_id, sigP2, sigP4, phobius ,RxLR_motif, RxLR_hmm, WY_hmm, CRN_LFLAK, CRN_DWL, orthogroup]
+    outline.extend(split_line)
+    outline.append(prot_seq)
+    print "\t".join(outline)
