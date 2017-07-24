d5cd0a54f2d41260fe29a2887b334488ff7ef755
adarmitage <Andrew.Armitage@EMR.ac.uk>
2017-07-24 13:54:45 +0100

    Tidied script

--- Pcac_popgen/vcf_2circosplot.py
+++ Pcac_popgen/vcf_2circosplot.py
@@ -15,34 +15,21 @@ from sets import Set
 #-----------------------------------------------------
 ap = argparse.ArgumentParser()
 ap.add_argument('--inp_vcf',required=True,type=str,help='input vcf file')
-# ap.add_argument('--gff_windows',required=False,type=str,help='gff file giving genomic windows over which SNPs will be recorded')
-# ap.add_argument('--per_X_bp',required=False, default=1000, type=float, help='The number of bp over which you want to report the number of features eg. SNPs per 1000bp')
-
-
-conf = ap.parse_args() #sys.argv
-# multiplier = conf.per_X_bp
-#
-# if conf.gff_windows:
-#     windows = conf.conf.gff_windows
+conf = ap.parse_args()
 
 with open(conf.inp_vcf) as f:
     inp_lines = f.readlines()
 
-
 #-----------------------------------------------------
 # Step 2
-# Order gff features by input contigs and gene start
+# print locations of SNPs as circos scatterplot values
 #-----------------------------------------------------
 
 contig_list = []
-# gene_start_dict = defaultdict(list)
-# features_dict = defaultdict(list)
-# conversion_dict = defaultdict(list)
 
 for line in inp_lines:
     line = line.strip("\n")
     if line.startswith("#"):
-        # print line
         continue
     split_line = line.split()
     contig = split_line[0]
