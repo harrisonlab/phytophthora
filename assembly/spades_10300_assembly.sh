5278a4d9fe17e0c036c76b1212893406bc17f358
adarmitage <Andrew.Armitage@EMR.ac.uk>
2017-10-19 13:33:24 +0100

    use of mateclip trimmed reads and shorter kmer

--- assembly/spades_10300_assembly.sh
+++ assembly/spades_10300_assembly.sh
@@ -32,14 +32,15 @@ Lib3R=$(ls qc_dna/paired/P.cactorum/10300/R/*.fq.gz | grep -v 'Pcact10300_S2_L00
 Lib4F=$(ls qc_dna/paired/P.cactorum/10300/F/*.fq.gz | grep -v 'Pcact10300_S2_L001_R1_001.fastq.gz' | head -n4 | tail -n1)
 Lib4R=$(ls qc_dna/paired/P.cactorum/10300/R/*.fq.gz | grep -v 'Pcact10300_S2_L001_R2_001.fastq.gz' | head -n4 | tail -n1)
 # Lib5InsLgth=5000
-Lib5F=$(ls qc_dna/mate-paired/P.cactorum/10300/F/*.fq.gz | grep -v 'rev' | head -n1 | tail -n1)
-Lib5R=$(ls qc_dna/mate-paired/P.cactorum/10300/R/*.fq.gz | grep -v 'rev' | head -n1 | tail -n1)
-
+# Lib5F=$(ls qc_dna/mate-paired/P.cactorum/10300/F/*.fq.gz | grep -v 'rev' | head -n1 | tail -n1)
+# Lib5R=$(ls qc_dna/mate-paired/P.cactorum/10300/R/*.fq.gz | grep -v 'rev' | head -n1 | tail -n1)
+Lib5F=$(ls qc_dna/mate-paired/P.cactorum/10300/nextclip_no_rev/P.cactorum_10300_trim_clip_D_R1.fastq.gz)
+Lib5R=$(ls qc_dna/mate-paired/P.cactorum/10300/nextclip_no_rev/P.cactorum_10300_trim_clip_D_R2.fastq.gz)
 
 Strain=10300
 Organism=P.cactorum
 WorkDir=$TMPDIR/"$Strain"_assembly
-OutDir=$CurPath/assembly/spades/$Organism/$Strain
+OutDir=$CurPath/assembly/spades2/$Organism/$Strain
 Cutoff="auto"
 AssemblyName="$Strain"_spades
 
@@ -74,25 +75,42 @@ cp $CurPath/$Lib5R Lib5_2.fq.gz
 
 # abyss-pe k=64 -np 16 -j 16 name=P.cac_10300 lib='pe1 pe2 pe3 pe4 mp5' pe1='Lib1_1.fq.gz Lib1_2.fq.gz' pe2='Lib2_1.fq.gz Lib2_2.fq.gz' pe3='Lib3_1.fq.gz Lib3_2.fq.gz' pe4='Lib4_1.fq.gz Lib4_2.fq.gz' mp5='Lib5_1.fq.gz Lib5_2.fq.gz'
 
+spades.py \
+    -k 21,33,45 \
+    -m 200 \
+    --phred-offset 33 \
+    --pe1-1 Lib1_1.fq.gz \
+    --pe1-2 Lib1_2.fq.gz \
+    --pe2-1 Lib2_1.fq.gz \
+    --pe2-2 Lib2_2.fq.gz \
+    --pe3-1 Lib3_1.fq.gz \
+    --pe3-2 Lib3_2.fq.gz \
+    --pe4-1 Lib4_1.fq.gz \
+    --pe4-2 Lib4_2.fq.gz \
+    --mp1-1 Lib5_1.fq.gz \
+    --mp1-2 Lib5_2.fq.gz \
+    -t 16  \
+    -o $WorkDir/. \
+    --cov-cutoff "$Cutoff"
 
-    spades.py \
-        -k 21,33,45,57 \
-        -m 200 \
-        --phred-offset 33 \
-        --pe1-1 Lib1_1.fq.gz \
-        --pe1-2 Lib1_2.fq.gz \
-        --pe2-1 Lib2_1.fq.gz \
-        --pe2-2 Lib2_2.fq.gz \
-        --pe3-1 Lib3_1.fq.gz \
-        --pe3-2 Lib3_2.fq.gz \
-        --pe4-1 Lib4_1.fq.gz \
-        --pe4-2 Lib4_2.fq.gz \
-        --mp1-fr \
-        --mp1-1 Lib5_1.fq.gz \
-        --mp1-2 Lib5_2.fq.gz \
-        -t 16  \
-        -o $WorkDir/. \
-        --cov-cutoff "$Cutoff"
+    # spades.py \
+    #     -k 21,33,45,57 \
+    #     -m 200 \
+    #     --phred-offset 33 \
+    #     --pe1-1 Lib1_1.fq.gz \
+    #     --pe1-2 Lib1_2.fq.gz \
+    #     --pe2-1 Lib2_1.fq.gz \
+    #     --pe2-2 Lib2_2.fq.gz \
+    #     --pe3-1 Lib3_1.fq.gz \
+    #     --pe3-2 Lib3_2.fq.gz \
+    #     --pe4-1 Lib4_1.fq.gz \
+    #     --pe4-2 Lib4_2.fq.gz \
+    #     --mp1-fr \
+    #     --mp1-1 Lib5_1.fq.gz \
+    #     --mp1-2 Lib5_2.fq.gz \
+    #     -t 16  \
+    #     -o $WorkDir/. \
+    #     --cov-cutoff "$Cutoff"
 
 echo "Filtering contigs smaller than 500bp"
 mkdir -p $WorkDir/filtered_contigs
