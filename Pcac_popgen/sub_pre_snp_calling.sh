dce881406f2fd631ae6a1ae94063fe1b5a82b6a9
adarmitage <Andrew.Armitage@EMR.ac.uk>
2018-03-12 13:40:01 +0000

    Now deals with Bam format inputs lacking headers as receieved from bowtie

--- Pcac_popgen/sub_pre_snp_calling.sh
+++ Pcac_popgen/sub_pre_snp_calling.sh
@@ -1,8 +1,8 @@
 #$ -S /bin/bash
 #$ -cwd
 #$ -pe smp 8
-#$ -l h_vmem=2G
-#$ -l h=blacklace01.blacklace|blacklace02.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+#$ -l virtual_free=1G
+#$ -l h=blacklace02.blacklace|blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
 ##############################################
 # Prep mappings from Bowtie2 for SNP calling
 ### Remove multimapping reads, discordant reads. PCR and optical duplicates, and
@@ -16,7 +16,7 @@
 
 InputSam=$1
 Prefix=$2
-# Filename=$(basename "$InputSam")
+Filename=$(basename "$InputSam")
 OutDir=$(dirname "$InputSam")
 Name="${Filename%.*}"
 
@@ -29,20 +29,41 @@ mkdir -p $WorkDir
 cp $InputSam $WorkDir/.
 cd $WorkDir
 
+
+### If the provided file is a .bam file then convert it into sam format.
+
+Extension=$(echo $Filename | rev | cut -f1 -d '.' | rev)
+if [[ $Extension == "bam" ]]; then
+  echo ".bam file extension found"
+  echo "converting to sam"
+  samtools view --threads 8 -o $Name.sam $CurDir/$InputSam
+  echo "Reheadering the bam file using the original sam headers"
+  samtools view -H $CurDir/$InputSam > header.sam
+  cat header.sam $Name.sam > ${Name}_reheader.sam
+  Filename=${Name}_reheader.sam
+fi
+
 ### Get rid of multimapping reads by filtering out on the XS:i: tag
-grep -v "XS:i" *.sam > tmp.sam
+echo "Removing reads with the XS:i flag (multimapping reads)"
+grep -v "XS:i" $Filename > tmp.sam
 # mv tmp.sam $Filename
+echo "Converting sam to bam format"
 samtools view --threads 8 -bS -o $Name.bam tmp.sam
+
+echo "Sorting and indexing the bam file"
 samtools sort --threads 8 -o "$Name"_nomulti_sorted.bam $Name.bam
 samtools index -@ 8 "$Name"_nomulti_sorted.bam "$Name"_nomulti_sorted.bam.index
 
 ### Keep only reads with "paired reads" and "properly paired reads" flags.
+echo "Filtering reads to retain only concordant paired reads"
 samtools view -b -h -f 3 -o "$Name"_nomulti_proper.bam "$Name"_nomulti_sorted.bam
 ### Sort for downstream analyses
+"Sorting and indexing concordant, paired reads bam file"
 samtools sort --threads 8 -o "$Name"_nomulti_proper_sorted.bam "$Name"_nomulti_proper.bam
 samtools index -@ 8 "$Name"_nomulti_proper_sorted.bam "$Name"_nomulti_proper_sorted.bam.index
 
 ### Remove PCR and optical duplicates
+echo "Using picard tools to remove duplicate reads"
 ProgDir=/home/sobczm/bin/picard-tools-2.5.0
 java -jar $ProgDir/picard.jar MarkDuplicates \
   INPUT="$Name"_nomulti_proper_sorted.bam \
@@ -51,6 +72,7 @@ java -jar $ProgDir/picard.jar MarkDuplicates \
   REMOVE_DUPLICATES=TRUE ASSUME_SORTED=TRUE MAX_RECORDS_IN_RAM=500000000 \
   MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 VALIDATION_STRINGENCY=LENIENT
 ### Add group and sample name (Prefix)
+echo "Adding the given sample information to the BAM file: $Prefix"
 java -jar $ProgDir/picard.jar AddOrReplaceReadGroups \
   INPUT="$Name"_nomulti_proper_sorted_nodup.bam \
   OUTPUT="$Name"_nomulti_proper_sorted_nodup_rg.bam \
