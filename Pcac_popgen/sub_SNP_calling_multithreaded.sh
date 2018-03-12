4c5c29e3dfd1cfea56a5310f45df1d703f1d43d7
adarmitage <Andrew.Armitage@EMR.ac.uk>
2018-03-12 13:50:58 +0000

    Edited for publication alignments

--- Pcac_popgen/sub_SNP_calling_multithreaded.sh
+++ Pcac_popgen/sub_SNP_calling_multithreaded.sh
@@ -14,9 +14,11 @@
 # read group added) are to be used in SNP calling, use the -I argument with full path to each file following after that.
 # Each new BAM file has to be specified after a separate -I
 
-Project=/home/groups/harrisonlab/project_files/idris
+# Project=/home/groups/harrisonlab/project_files/idris
+Project=/scratch/data/armita/idris
 OutDir=analysis/popgen/SNP_calling
-Reference=$(ls /home/groups/harrisonlab/project_files/idris/repeat_masked/P.cactorum/414_v2/filtered_contigs_repmask/414_v2_contigs_unmasked.fa)
+# Reference=$(ls /home/groups/harrisonlab/project_files/idris/repeat_masked/P.cactorum/414/filtered_contigs_repmask/414_contigs_unmasked.fa)
+Reference=$(ls /data/scratch/armita/idris/repeat_masked/P.cactorum/414/filtered_contigs_repmask/414_contigs_unmasked.fa)
 
 RefName=$(basename "$Reference")
 Out1="${RefName%.*}_temp.vcf"
@@ -30,23 +32,26 @@ java -jar $ProgDir/GenomeAnalysisTK.jar \
      -ploidy 2 \
      -nct 24 \
      --allow_potentially_misencoded_quality_scores \
-     -I $Project/analysis/popgen/P.cactorum/12420/12420_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/15_13/15_13_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/15_7/15_7_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/2003_3/2003_3_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/4032/4032_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/4040/4040_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/404/404_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/414/414_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/415/415_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/416/416_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/62471/62471_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/P295/P295_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/PC13_15/PC13_15_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.cactorum/R36_14/R36_14_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.idaei/371/371_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.idaei/SCRP370/SCRP370_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
-     -I $Project/analysis/popgen/P.idaei/SCRP376/SCRP376_vs_414_v2_aligned_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/11-40/11-40_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/12420/12420_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/15_13/15_13_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/15_7/15_7_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/17-21/17-21_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/2003_3/2003_3_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/4032/4032_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/4040/4040_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/404/404_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/414/414_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/415/415_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/416/416_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/62471/62471_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/P295/P295_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/P421/P421_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/PC13_15/PC13_15_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.cactorum/R36_14/R36_14_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.idaei/371/371_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.idaei/SCRP370/SCRP370_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
+     -I $Project/analysis/popgen/P.idaei/SCRP376/SCRP376_vs_414_aligned_sorted_nomulti_proper_sorted_nodup_rg.bam \
      -o $Out1
 
 #Break down complex SNPs into primitive ones with VariantsToAllelicPrimitives
