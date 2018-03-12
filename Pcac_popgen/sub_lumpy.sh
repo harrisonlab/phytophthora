4b1832c6ee40727a0faf5781009ef8bb76528b17
adarmitage <Andrew.Armitage@EMR.ac.uk>
2018-03-12 13:39:13 +0000

    minor edits

--- Pcac_popgen/sub_lumpy.sh
+++ Pcac_popgen/sub_lumpy.sh
@@ -2,7 +2,7 @@
 #$ -cwd
 #$ -pe smp 1
 #$ -l h_vmem=1G
-#$ -l h=blacklace06.blacklace
+#$ -l h=blacklace03.blacklace|blacklace04.blacklace|blacklace05.blacklace|blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
 ###
 
 #Input: Just the name for Output results files. The script needs to be run from the directory containing all the Output files from sub_bwa_mem.sh for the samples of interest (but not any others!!)
@@ -10,7 +10,6 @@
 #Output: Genotype (*.gt) and VCF (*.vcf) files with structural variants.
 
 Prefix=$1
-Strain=$2
 
 #First, obtain command line arguments with: 1) all the alignments
 # 2) discordant alignments, 3) split alignments to be used as input
@@ -18,28 +17,28 @@ Strain=$2
 
 #1)
 Bams=()
-for b in $(ls *_sorted.bam | grep -v -e 'splitters' -e 'discordant' | grep "${Strain}_"); do
+for b in $(ls *_sorted.bam | grep -v -e 'splitters' -e 'discordant'); do
   Bams+=("$b");
 done;
 Bam=$(IFS=, ; echo "${Bams[*]}")
 
 #2)
 Discs=()
-for b in $(ls *discordants_sorted.bam | grep "${Strain}_"); do
+for b in $(ls *discordants_sorted.bam); do
   Discs+=("$b");
 done;
 Discordant=$(IFS=, ; echo "${Discs[*]}")
 
 #3)
 Splits=()
-for b in $(ls *splitters_sorted.bam | grep "${Strain}_"); do
+for b in $(ls *splitters_sorted.bam); do
   Splits+=("$b");
 done;
 Splitters=$(IFS=, ; echo "${Splits[*]}")
 
 # Out="$Prefix".gt
 
-echo "Strain: $Strain"
+# echo "Strain: $Strain"
 echo "Bams: $Bams"
 echo "Discordant: $Discordant"
 echo "Splitters: $Splitters"
